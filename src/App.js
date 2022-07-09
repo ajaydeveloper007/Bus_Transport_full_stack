1cd26f29460e506ac623fbb7a348d76e9236e39b
Ajay Kumar Yadav <32960809+ajaydeveloper007@users.noreply.github.com>
2022-07-09 07:58:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './login.js'
+import './registration.js'
+
+
+function App() {
+  return (
+    <div className="App">
+    <RegistrationForm/>
+    <FormPage/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/transport.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const transport = async() => {
+    fetch('https://transportapi.com/v3/uk/bus/route/FPOT/72/outbound/3800C508601/2021-06-01/07:11/timetable.json')
+}
+
